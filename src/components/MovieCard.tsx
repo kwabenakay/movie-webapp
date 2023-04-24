32e454d11920f14a1b693305e24bfdc72c1bf226
kwabenakay <nskwbn@gmail.com>
2023-04-24 09:48:29 +0000

    Designed the mobile view of the movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,14 +7,14 @@ export default function MovieCard() {
     setData([...data.filter((movie) => movie.isTrending)]);
   }, []);
   return (
-    <div className=" grid grid-cols-2">
-      {data.map((movie) => (
-        <div>
+    <div className=" grid grid-cols-2 gap-4 tablet:grid-cols-3">
+      {data.map((movie,ind) => (
+        <div key={movie.title+ind} className=" relative w-fit">
             <div className=" absolute left-3/4">
               {movie.isBookmarked ? "booked" : "not booked"}
             </div>
-            <img src={movie.thumbnail.regular.small} alt="" />
-            <div>
+            <img className=" rounded-lg" src={movie.thumbnail.regular.small} alt="" />
+            <div className=" text-xs">
             <span>{movie.year}</span>
             <span>{movie.category}</span>
             <span>{movie.rating}</span>
